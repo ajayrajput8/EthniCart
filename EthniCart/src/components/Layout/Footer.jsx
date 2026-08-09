07aef000b538d9eb317b4afe241c80cfcc9c13db
vijaysingh2002 <vijaysinghdarbarr@gmail.com>
2026-08-10 00:42:13 +0530

    stable working EthniCart before cleanup

--- EthniCart/src/components/Layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="py-10 bg-black text-white text-center">
-      © 2026 EthniCart. All rights reserved.
-    </footer>
-  );
-};
-
-export default Footer;
